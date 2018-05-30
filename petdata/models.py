6e0030b5965ead82cbff458449cda9d6078b190b
linzh17 <15018279123@163.com>
2018-05-30 21:52:32 +0800

    edit1

--- petdata/models.py
+++ petdata/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass
+class Pet(models.Model):
+    pet_id = models.IntegerField(primary_key=True)
+    user_list = models.ForeignKey(User)
+
+class Pet_State(models.Model):
+     pet_id = models.ForeignKey(Pet)
+     pet_clean = models.IntegerField()
+     pet_hunger = models.IntegerField()    
